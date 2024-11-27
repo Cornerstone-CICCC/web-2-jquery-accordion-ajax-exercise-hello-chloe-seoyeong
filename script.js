bc5932c0efaf03e14cd600093e34bb6ea2a423a0
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-11-27 11:24:35 -0800

    Completed jQuery accordion ajax exercise

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 $(function() {
   // your code here
+  // Exercise 1
+  $('.accordion-header').on('click', function() {
+    $('.accordion-content').slideUp();
+    $(this).next().slideDown();
+  });
+
+  // Exercise 2
+  $('.todos').find('button').on('click', function() {
+    $.ajax({
+      url: 'https://dummyjson.com/todos',
+      type: 'GET',
+      success: function(response) {
+        const todos = response.todos;
+        console.log(todos);
+        todos.forEach(todo => {
+          $('.todos').find('ul').append(
+            `<li>${todo.todo}</li>`
+          )
+        });
+      },
+      error: function(err) {
+        console.error(err);
+      }
+    })
+  });
 })
